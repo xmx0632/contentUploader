7f7c29f93194e3fbed07a0a19ac1003e258fb5cd
xmx0632 <xmx0632@gmail.com>
2025-02-28 09:54:45 +0800

    添加部署脚本

---
+++ wechat-video-uploader/deploy.sh
@@ -0,0 +1,6 @@
+# 自动把当前目录下的 wechat-video-uploader-macos-arm64 复制到4个子目录： douyin kuaishou weixin rednote 下，覆盖原有文件
+cp -rf wechat-video-uploader-macos-arm64 douyin
+cp -rf wechat-video-uploader-macos-arm64 kuaishou
+cp -rf wechat-video-uploader-macos-arm64 weixin
+cp -rf wechat-video-uploader-macos-arm64 rednote
+
